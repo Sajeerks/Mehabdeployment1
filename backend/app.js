8a52786ef4543d35129d9e63367e6358f182eda6
sajeer <sajeersayed@gmail.com>
2023-01-26 17:04:10 +0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const errorMiddleware = require("./middleware/error.js")
 const bodyParser = require("body-parser")
 const fileUpload = require("express-fileupload")
 dotenv.config({path:'backend/config/config.env'})
+const path = require("path")
 
 
 app.use(express.json({ limit: "50mb" }));
@@ -28,14 +29,21 @@ const paymentRoute = require("./routes/paymentRoute")
 
 
 
+
+
 app.use('/api/v1', productRoute)
 app.use('/api/v1', userRoute)
 app.use('/api/v1', orderRoute)
 app.use('/api/v1', paymentRoute)
 
 
+//after npm run build
+app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html" ))
+})
+///////
 
 
 app.use(errorMiddleware)
