c8b2ea07f64a1515c511fcb0ca44c7eebc684a56
sajeer <sajeersayed@gmail.com>
2023-01-29 10:58:31 +0300

    dddd

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,10 @@ const bodyParser = require("body-parser")
 const fileUpload = require("express-fileupload")
 dotenv.config({path:'backend/config/config.env'})
 const path = require("path")
+const cors = require("cors");
 
+
+app.use(cors())
 
 app.use(express.json({ limit: "50mb" }));
  app.use(express.urlencoded({ limit: "50mb", extended: true }));
