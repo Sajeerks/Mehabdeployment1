77aa0b0171b1223e984a0c6f26ab2a909e0903ea
sajeer <sajeersayed@gmail.com>
2023-01-29 09:16:05 +0300

    cnagedd

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv')
 const connect_database = require("./config/database")
 const cloudinary = require("cloudinary")
 
-const PORT = process.env.PORT || 4000
+// const PORT = process.env.PORT || 4000
+const PORT =  4000
+
 
 //config
 dotenv.config({path:'backend/config/config.env'})
